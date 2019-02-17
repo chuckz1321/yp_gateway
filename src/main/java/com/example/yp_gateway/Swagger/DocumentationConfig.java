dbc72c670a96fd8c9238629c320c4af55b0dcba2
zcy960312 <zcy960312@gmail.com>
2019-02-17 18:49:18 -0500

    mix all apis

---
+++ src/main/java/com/example/yp_gateway/Swagger/DocumentationConfig.java
@@ -0,0 +1,30 @@
+package com.example.yp_gateway.Swagger;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Primary
+public class DocumentationConfig implements SwaggerResourcesProvider {
+    @Override
+    public List<SwaggerResource> get() {
+        List resources = new ArrayList();
+        resources.add(swaggerResource("skiresort", "/ski/v2/api-docs", "2.0"));
+        resources.add(swaggerResource("museum", "/museum/v2/api-docs", "2.0"));
+        resources.add(swaggerResource("restaurant", "/rest/v2/api-docs", "2.0"));
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+}
