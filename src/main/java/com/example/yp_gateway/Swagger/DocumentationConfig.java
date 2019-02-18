54fd50328beee382aa7d50916f550f633aa48837
zcy960312 <zcy960312@gmail.com>
2019-02-18 16:27:38 -0500

    update swagger dependency

--- src/main/java/com/example/yp_gateway/Swagger/DocumentationConfig.java
+++ src/main/java/com/example/yp_gateway/Swagger/DocumentationConfig.java
@@ -1,19 +1,30 @@
 package com.example.yp_gateway.Swagger;
 
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
 @Primary
 public class DocumentationConfig implements SwaggerResourcesProvider {
+
+//    private RouteLocator routeLocator;
+//
+//    public DocumentationConfig(RouteLocator routeLocator) {
+//        this.routeLocator = routeLocator;
+//    }
+
     @Override
     public List<SwaggerResource> get() {
         List resources = new ArrayList();
+
         resources.add(swaggerResource("skiresort", "/ski/v2/api-docs", "2.0"));
         resources.add(swaggerResource("museum", "/museum/v2/api-docs", "2.0"));
         resources.add(swaggerResource("restaurant", "/rest/v2/api-docs", "2.0"));
--- src/main/java/com/example/yp_gateway/Swagger/SwaggerConfig.java
+++ src/main/java/com/example/yp_gateway/Swagger/SwaggerConfig.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -24,5 +25,11 @@ public class SwaggerConfig {
                 .termsOfServiceUrl("http://www.google.com")
                 .version("1.0")
                 .build();
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return new UiConfiguration(null, "list", "alpha", "schema",
+                UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS, false, true, 60000L);
     }
 }
